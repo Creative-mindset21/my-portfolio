8e58c2eafd7032cdeb183e8eabe4b4af543c9e53
Creative Mindset <tobiawotoye03@gmail.com>
2024-12-14 19:52:14 +0000

    implemented the menu functionality

--- script.js
+++ script.js
@@ -102,3 +102,27 @@ function scrollUp() {
 }
 
 window.addEventListener("scroll", scrollUp);
+
+//! MENU FUNCTIONALITY
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+const navLink = document.querySelectorAll(".nav-link");
+
+function linkAction() {
+  navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
