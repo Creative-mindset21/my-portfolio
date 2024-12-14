dd078c34fc54cc508221bb87a08f53744fbd159b
Creative Mindset <tobiawotoye03@gmail.com>
2024-12-14 03:09:32 +0000

    made the scrollup button functionable

--- script.js
+++ script.js
@@ -89,3 +89,16 @@ function scrollHeader() {
 }
 
 window.addEventListener("scroll", scrollHeader);
+
+//! SHOW SCROLL UP BUTTON
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+
+  if (this.scrollY >= 350) {
+    scrollUp.classList.add("show-scroll");
+  } else {
+    scrollUp.classList.remove("show-scroll");
+  }
+}
+
+window.addEventListener("scroll", scrollUp);
