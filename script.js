5a87cdd4518a3e725c91452bd68033eb1ffcbc79
Creative Mindset <tobiawotoye03@gmail.com>
2024-12-14 02:57:48 +0000

    created a slide-down function

--- script.js
+++ script.js
@@ -76,3 +76,16 @@ const footerCopyright = document.getElementById("footer-copyright");
 footerCopyright.innerHTML = `&copy; ${today.getFullYear()}. All rights reserved`;
 
 console.log(footerCopyright);
+
+//! BACKGROUND HEADER
+function scrollHeader() {
+  const header = document.getElementById("header");
+
+  if (this.scrollY >= 80) {
+    header.classList.add("scroll-header");
+  } else {
+    header.classList.remove("scroll-header");
+  }
+}
+
+window.addEventListener("scroll", scrollHeader);
