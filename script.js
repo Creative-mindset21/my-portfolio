579afe9499641b44247122736a7ccc4e388c5a30
Creative Mindset <tobiawotoye03@gmail.com>
2024-12-14 00:07:55 +0000

    made the contact form send a result to my personal email using email.js

--- script.js
+++ script.js
@@ -18,3 +18,53 @@ tabs.forEach((tab) => {
     tab.classList.add("active");
   });
 });
+
+//! CONTACT FORM
+const contactForm = document.getElementById("contact-form");
+const contactName = document.getElementById("name");
+const contactEmail = document.getElementById("email");
+const contactSubject = document.getElementById("subject");
+const contactMessage = document.getElementById("message");
+const errorMessage = document.getElementById("error-message");
+
+errorMessage.textContent = "Input required fields";
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (
+    contactName.value === "" ||
+    contactEmail.value === "" ||
+    contactSubject.value === "" ||
+    contactMessage.value === ""
+  ) {
+    errorMessage.textContent = "Input required fields";
+    console.log(errorMessage);
+  } else {
+    emailjs
+      .sendForm(
+        "service_gojvbpv",
+        "template_489kuwd",
+        "#contact-form",
+        "tItnUrD_Ciq5V3pXn"
+      )
+      .then(
+        () => {
+          errorMessage.classList.add("color-first");
+          errorMessage.textContent = "Message sent ✔️";
+
+          setTimeout(() => {
+            errorMessage.textContent = "";
+          }, 5000);
+        },
+        (error) => {
+          alert("SOMETHING WENT WRONG...", error);
+        }
+      );
+
+    contactName.value = "";
+    contactEmail.value = "";
+    contactSubject.value = "";
+    contactMessage.value = "";
+  }
+});
